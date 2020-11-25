1814bd4271f5fadc2277ab84a2b23bf2bf36ec75
Florian <florianmarin77@yahoo.com>
2020-11-25 12:02:35 +0200

    Activate all Loaders including the necessary tests.

---
+++ src/test/java/com/asi/sda/sample/faker/SampleSimFakerTest.java
@@ -0,0 +1,12 @@
+package com.asi.sda.sample.faker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleSimFakerTest {
+
+    @Test
+    void createDummyList() {
+    }
+}
---
+++ src/test/java/com/asi/sda/sample/loader/SampleLineLoaderTest.java
@@ -0,0 +1,37 @@
+package com.asi.sda.sample.loader;
+
+import com.asi.sda.sample.Sample;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SampleLineLoaderTest {
+
+    @Test
+    void loadData() {
+        /**
+         * given => the resource file named bookListTest.csv
+         * when  => trying to load from this resource file
+         * then  => the result list contains expected items
+         */
+
+        SampleLoader sampleLoader = new SampleLineLoader();
+        List<Sample> samples = new ArrayList<>();
+        Path sampleListPath;
+        try {
+            sampleListPath = Paths.get(ClassLoader.getSystemResource("sample/sampleListTest.txt").toURI());
+            samples = sampleLoader.loadData(Paths.get(String.valueOf(sampleListPath)));
+        } catch (URISyntaxException exception) {
+            exception.printStackTrace();
+        }
+
+        assertThat(samples.get(0).getText()).isEqualTo("Az0123456789");
+        assertThat(samples.get(1).getText()).isEqualTo("Za9876543210");
+    }
+}
