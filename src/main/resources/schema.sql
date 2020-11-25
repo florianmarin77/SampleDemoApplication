881dbd156fa38cffd95f65cda4b1255ccb398cfb
Florian <florianmarin77@yahoo.com>
2020-11-25 12:30:12 +0200

    Create SDA database named macromedia.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE sda;
+CREATE DATABASE macromedia;
 
-CREATE TABLE `sda`.`sample` (
+CREATE TABLE `macromedia`.`sample` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `text` VARCHAR(30) NULL,
   PRIMARY KEY (`id`));
 
-USE anydew;
+USE macromedia;
