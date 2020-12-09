ec0a7165de80722c714e99747d4148b9a234c37b
Florian <florianmarin77@yahoo.com>
2020-12-09 10:57:25 +0200

    JDBC: add config class.

--- src/main/java/com/asi/sda/sample/config/JdbcConfig.java
+++ src/main/java/com/asi/sda/sample/config/JdbcConfig.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
