356765a2398799bc62d2fff1018aa9a5843fed01
Florian <florianmarin77@yahoo.com>
2020-12-14 09:46:02 +0200

    Add Hibernate configuration class.

---
+++ src/main/java/com/asi/sda/sample/config/HibernateConfig.java
@@ -0,0 +1,47 @@
+package com.asi.sda.sample.config;
+
+import com.asi.sda.sample.Sample;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateConfig {
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        Configuration configuration = createConfig();
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    private static Configuration createConfig() {
+        Configuration configuration = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/macromedia?serverTimezone=UTC");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+        configuration.setProperties(properties);
+
+        // add annotated classes
+        configuration.addAnnotatedClass(Sample.class);
+
+        return configuration;
+    }
+}
