bf22a054c032d05743cdee3c1e584830282504d1
Florian <florianmarin77@yahoo.com>
2021-01-06 16:48:05 +0200

    Minor update into SpringRepository.

--- src/main/java/com/asi/sda/sample/repository/SampleSpringRepository.java
+++ src/main/java/com/asi/sda/sample/repository/SampleSpringRepository.java
@@ -2,7 +2,9 @@ package com.asi.sda.sample.repository;
 
 import com.asi.sda.sample.model.Sample;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SampleSpringRepository extends CrudRepository<Sample, Integer> {
     // empty interface by default
 }
