91756499ce0565d950e37a53bb06a3bf63982958
Florian <florianmarin77@yahoo.com>
2021-01-08 08:28:04 +0200

    Rename Spring application.

--- src/main/java/com/asi/sda/SampleDemoApplication.java
+++ src/main/java/com/asi/sda/SampleDemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestApplication {
+public class SampleDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringRestApplication.class, args);
+        SpringApplication.run(SampleDemoApplication.class, args);
     }
 }
