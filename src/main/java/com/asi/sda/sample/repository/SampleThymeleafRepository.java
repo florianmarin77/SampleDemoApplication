a3b62f2a54598db4578f90c2ad513cf614333f88
Florian <florianmarin77@yahoo.com>
2021-01-08 08:47:57 +0200

    Add Thymeleaf repository.

---
+++ src/main/java/com/asi/sda/sample/repository/SampleThymeleafRepository.java
@@ -0,0 +1,10 @@
+package com.asi.sda.sample.repository;
+
+import com.asi.sda.sample.model.Sample;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SampleThymeleafRepository extends JpaRepository<Sample, Integer> {
+    // no custom methods by default
+}
